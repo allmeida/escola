90c4d5e6c420d867893f864280bc11842c121470
allmeida <allmeida.fran@gmail.com>
2021-06-19 22:57:04 -0400

    my-agenda - Corrigindo erro ao excluir

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,11 +3,28 @@
 @section('title', 'Dashboard')
 
 @section('content_header')
-    <h1>Dashboard</h1>
+
+<div class="container">
+    <div class="row">
+        <div class="col-lg-12">
+            <div>
+                <h1>FRANCINILDO DE FREITAS ALMEIDA</h1>
+                <h2>Analista de Sistemas  •  Programador Web</h2>
+            </div>
+            <hr />
+            <div class="btn">
+                <a href="https://www.linkedin.com/in/francinildoalmeida" target="_blanck" class="btn btn-rounded btn-blue" >Linkedin</a>
+                
+                <a href="https://github.com/allmeida/escola" target="_blanck" class="btn btn-rounded btn-blue" >Github</a>
+            </div>
+        </div>
+    </div> 
+</div>
+
 @stop
 
 @section('content')
-    <p>Welcome to this beautiful admin panel.</p>
+    
 @stop
 
 @section('css')
@@ -15,3 +32,42 @@
 
 @section('js')
 @stop
+
+<style>
+    
+.container {
+text-align:center;
+}
+
+.container h1 {
+    color: #314584;
+    font-weight: bold;
+    padding-top: 20px;
+    padding-bottom: 40px;
+    text-transform: uppercase;
+}
+
+.container h2 {
+    color: #314584;
+    padding-top: 10px;
+    padding-bottom: 30px;
+}
+
+.btn {
+    text-align:center;
+    margin-right: 20px;
+}
+
+.btn.btn-rounded.btn-blue {
+    background-color: #007bff;
+    border-radius: 15px;
+    color: #fff;
+    padding: 5px 20px;
+}
+
+.btn.btn-rounded.btn-blue:hover {
+    background-color: #fff;
+    color: #007bff;
+    border: 1px solid #007bff;
+}
+</style>
--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,18 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use Illuminate\Support\Facades\Route;
 
+Auth::routes([
+    'register' => false
+]);
 
+Route::middleware('auth')->group(function () {
+    Route::get('/', function () {
+        return view('home');
+    });
 
-Auth::routes();
-
-Route::get('/', 'HomeController@index')->name('home');
-Route::middleware('auth')->group( function() {
     Route::resource('cliente', 'ClienteController');
     Route::resource('formacao', 'FormacaoController');
 });
+
+Route::get('/home', 'HomeController@index')->name('home');
